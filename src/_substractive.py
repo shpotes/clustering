deaff467125a471afbfe8dc4692393a7670b7111
Santiago <shpotes3@gmail.com>
2020-09-20 17:09:55 -0500

    fix: add cosine similarity

--- src/_substractive.py
+++ src/_substractive.py
@@ -4,38 +4,87 @@ import jax.numpy as jnp
 from jax.ops import index, index_update
 import numpy as onp
 from sklearn.base import ClusterMixin, BaseEstimator
+from .utils import cosine_similarity
 
-@partial(jax.jit, static_argnums=(2, 3,))
-def mountain_function(x, d, norm_ord, r_a):
-    norm = partial(jnp.linalg.norm, ord=norm_ord)
-    return jnp.exp(-norm((d - x), axis=1) / ((r_a / 2) ** 2)).sum(axis=-1)
+@partial(jax.jit, static_argnums=(2,))
+def density_function(points, prototypes, r_a):
+    """
+    Compute the density meassure for the substractive clustering (Moertini, 2002)
+
+    Parameters
+    ----------
+    points : float[N, M]
+        Points to be clusters (N points, M dimensions)
+    prototypes : float[Np, M]
+        Clusters prototypes (N prototypes, M dimensions)
+    r_a : float
+        neighborhood radius
+
+    Returns
+    -------
+    type 
+        float[Np]
+    """
+    density = exp_dist(points, prototypes, r_a)
+    return jnp.sum(density, axis=0)
+
+@partial(jax.jit, static_argnums=(2,))
+def exp_dist(points, prototypes, radius):
+    """
+    Compute the density arround each prototype
+
+    Parameters
+    ----------
+    points : float[N, M]
+        Points to be clusters (N points, M dimensions)
+    prototypes : float[Np, M]
+        Clusters prototypes (N prototypes, M dimensions)
+    r_a : float
+        neighborhood radius
+
+    Returns
+    -------
+    type 
+        float[N, Np]
+    """
+
+    cosine_dist = 1 - cosine_similarity(points, prototypes, axis=1)
+    return jnp.exp(-cosine_dist / (radius / 2)**2)
 
 @jax.jit
 def get_cluster(x, density):
+    """
+    Return the next cluster
+
+    Parameters
+    ----------
+    x : float[Np, M]
+        Cluster candidates (Np points, M dimensions)
+    density : float[Np]
+        The precomputed density
+
+    Returns
+    -------
+    cluster: float[M]
+        The cluster that maximize the density
+    density : float
+        Their corresponding density
+    """
+
     cluster_idx = jnp.argmax(density)
     cluster = x[cluster_idx]
     return cluster, density[cluster_idx]
 
 @partial(jax.jit, static_argnums=(0,))
-def subtractive_update(params, val):
+def subtractive_update(r_a, val):
     idx, x, density, clusters, cluster_density = val
-    norm_ord, r_a = params
     r_b = 1.5 * r_a
 
-    cluster = clusters[idx]
+    cluster = clusters[idx][jnp.newaxis, :]
     idx += 1
 
-    cluster_mass = jnp.linalg.norm(
-        x - cluster.reshape(1, -1, 1),
-        ord=norm_ord, axis=1
-    ).sum()
-
-    near_cluster_density = cluster_mass * jnp.exp(
-        -jnp.linalg.norm(
-            x-  cluster.reshape(1, -1, 1),
-            ord=norm_ord, axis=1
-        )
-    )
+    cluster_mass = density_function(x, cluster, r_a)
+    near_cluster_density = cluster_mass * exp_dist(x, cluster, r_b)
 
     new_density = density - jnp.squeeze(near_cluster_density)
     
@@ -51,18 +100,15 @@ def subtractive_update(params, val):
 def stop(thresh, initial_state, state):
     return (state[-1] / initial_state[-1]) > thresh
 
-@partial(jax.jit, static_argnums=(1, 2, 3))
-def subtractive_run(x, norm_ord, r_a, thresh):
+@partial(jax.jit, static_argnums=(1, 2))
+def subtractive_run(x, r_a, thresh):
 
-    d = x.T[jnp.newaxis, ...]
-    x = x[..., jnp.newaxis]
-
-    density = mountain_function(x, d, norm_ord, r_a)
+    density = density_function(x, x, r_a)
     cluster, cluster_density = get_cluster(x, density)
 
     idx = 0
-    params = (norm_ord, r_a)
 
+    # Hacky dynamic storage in jax
     clusters = jnp.squeeze(jnp.zeros_like(x)) + jnp.nan
 
     clusters = index_update(
@@ -71,24 +117,22 @@ def subtractive_run(x, norm_ord, r_a, thresh):
         jnp.squeeze(cluster)
     )
 
-    val = (idx, x, density, clusters, cluster_density)
+    initial_state = (idx, x, density, clusters, cluster_density)
 
     state = jax.lax.while_loop(
-        partial(stop, thresh, val),
-        partial(subtractive_update, params),
-        val
+        partial(stop, thresh, initial_state),
+        partial(subtractive_update, r_a),
+        initial_state
     )
 
     return state
 
 
 class Substractive(ClusterMixin, BaseEstimator):
-    def __init__(self, norm_ord=2, r_a=0.1, precision=16,
+    def __init__(self, r_a=2, precision=16,
                  tol=0.1, random_state=42):
 
         assert precision in {16, 32}, 'wrong precision'
-
-        self.norm_ord = norm_ord
         self.r_a = r_a
         self.tol = tol
 
@@ -99,11 +143,12 @@ class Substractive(ClusterMixin, BaseEstimator):
         x = jnp.array(X, dtype=self._dtype)
 
         idx, x, density, clusters, cluster_density  = subtractive_run(
-            x, self.norm_ord, self.r_a, self.tol
+            x, self.r_a, self.tol
         )
 
+        self.n_clusters = idx + 1
         self.density = density
-        self.clusters = clusters[:idx+1]
+        self.clusters = clusters[:self.n_clusters]
         self.cluster_density = cluster_density
 
         return self
@@ -115,8 +160,10 @@ class Substractive(ClusterMixin, BaseEstimator):
         X = jnp.array(X, dtype=self._dtype)
 
         norm = partial(jnp.linalg.norm, ord=self.norm_ord)
+        
         assignment = jax.vmap(
             lambda points: jnp.argmin(jax.vmap(norm)(self.clusters - points))
         )(X)
+
         self.labels_ = onp.array(assignment)
         return assignment
