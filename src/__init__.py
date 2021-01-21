5b131fd785bf113d06d34cdaa51bf538000fcec1
Santiago <shpotes3@gmail.com>
2021-01-20 23:35:47 -0500

    fix import issue

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from ._kmeans import KMeans, KMeansPlusPlus
+from ._kprototypes import KPrototypes
 from ._fuzzy import CMeans
 from ._mountain import Mountain
 from ._substractive import Substractive
